a7c3255168488a8c0c34b4a45ff3285bcd8f64e0
Oleksandr Mul <mulalex834@gmail.com>
2021-12-12 00:33:18 +0100

    Calling the API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css'
+import { useState } from 'react'
+import Axios from 'axios'
 
 function App() {
+  const YOUR_APP_ID = 'c5c19023'
+  const YOUR_APP_KEY = '3c8f5fc7d905bb15cc0ac71417e1f8ad'
+  const [recepies, setRecepies] = useState([])
+
+  const url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+
+  const getRecipeInfo = async () => {
+    let result = await Axios.get(url)
+    setRecepies(result.data.hits)
+    console.log(result.data.hits);
+   
+  }
+
   return (
     <div className="app">
-      <h1>
+      <h1 onClick={getRecipeInfo}>
         <u>Food Recipe Hub</u>🥗
       </h1>
       <div className="app__searchForm">
@@ -14,9 +29,9 @@ function App() {
           className="app__input"
         />
         <select className="app__healthLabels">
-          <option value='vegan'>vegan</option>
+          <option value="vegan">vegan</option>
         </select>
-        <input className='app__submit' type='submit' value='Get Recipe'/>
+        <input className="app__submit" type="submit" value="Get Recipe" />
       </div>
     </div>
   )
