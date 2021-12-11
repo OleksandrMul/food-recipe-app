aafce9af9615dad59ecfc1d996b2761aa87216c8
Oleksandr Mul <mulalex834@gmail.com>
2021-12-11 22:24:40 +0100

    Create the basic structure&Styling the input&search field

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-     Hello
+    <div className="app">
+      <h1>
+        <u>Food Recipe Hub</u>🥗
+      </h1>
+      <div className="app__searchForm">
+        <input
+          type="text"
+          placeholder="Type the Ingredient"
+          autoComplete="Off"
+          className="app__input"
+        />
+        <select className="app__healthLabels">
+          <option value='vegan'>vegan</option>
+        </select>
+        <input className='app__submit' type='submit' value='Get Recipe'/>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
