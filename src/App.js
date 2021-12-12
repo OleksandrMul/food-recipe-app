2da032854ecbed551ae11181f6d9a492216f3658
Oleksandr Mul <mulalex834@gmail.com>
2021-12-12 11:41:00 +0100

    Implementing custom search

--- src/App.js
+++ src/App.js
@@ -5,34 +5,48 @@ import Axios from 'axios'
 function App() {
   const YOUR_APP_ID = 'c5c19023'
   const YOUR_APP_KEY = '3c8f5fc7d905bb15cc0ac71417e1f8ad'
+  const [query, setQuery] = useState('')
+  const [healthLabel, setHealthLabel] = useState('vegetarian')
   const [recepies, setRecepies] = useState([])
 
-  const url = `https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=${healthLabel}`
 
   const getRecipeInfo = async () => {
     let result = await Axios.get(url)
     setRecepies(result.data.hits)
-    console.log(result.data.hits);
-   
+    console.log(result.data.hits)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    getRecipeInfo()
   }
 
   return (
     <div className="app">
-      <h1 onClick={getRecipeInfo}>
+      <h1>
         <u>Food Recipe Hub</u>🥗
       </h1>
-      <div className="app__searchForm">
+      <form
+        type="submit"
+        onSubmit={(event) => onSubmit(event)}
+        className="app__searchForm"
+      >
         <input
           type="text"
           placeholder="Type the Ingredient"
           autoComplete="Off"
           className="app__input"
+          value={query}
+          onChange={(event) => {
+            setQuery(event.target.value)
+          }}
         />
         <select className="app__healthLabels">
           <option value="vegan">vegan</option>
         </select>
         <input className="app__submit" type="submit" value="Get Recipe" />
-      </div>
+      </form>
     </div>
   )
 }
